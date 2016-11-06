67e2117c9abca2c40ef21808d9a6de302c022068
Aaron Norby <aaron.norby@gmail.com>
2016-11-05 17:07:05 -0700

    Add endpoint for maintenance events

--- bikelog/__init__.py
+++ bikelog/__init__.py
@@ -10,12 +10,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from .models import Bike, MaintenanceEvent
-from .views.sessions import sessions
+from .views.sessions import sessions as sessions_view
 from .api.bikes import bikes
+from .api.maintenance_events import maint_events
 
 @app.route('/')
 def index():
     return "hellow wrld"
 
-app.register_blueprint(sessions)
+app.register_blueprint(sessions_view)
 app.register_blueprint(bikes, url_prefix='/api')
+app.register_blueprint(maint_events, url_prefix='/api')
---
+++ bikelog/api/maintenance_events.py
@@ -0,0 +1,80 @@
+from datetime import datetime, date
+from flask import Blueprint, request
+from flask_restful import Resource, Api, fields, marshal_with
+
+from bikelog import db
+from bikelog.models import MaintenanceEvent, Bike
+from bikelog.errors import ClientDataError
+
+maint_events = Blueprint('maintenance_events', __name__)
+maint_events_api = Api(maint_events)
+
+class EventDate(fields.Raw):
+    def format(self, value):
+        return value.strftime("%Y-%m-%d")
+
+@maint_events_api.resource('/maintenance_events/<int:bike_id>',
+        '/maintenance_events')
+class MaintenanceEventsApi(Resource):
+    """
+    Endpoint for interacting with maintenance events for a specific bike.
+    """
+
+    resource_fields = {
+        'id': fields.Integer,
+        'date': EventDate,
+        'description': fields.String,
+        'note': fields.String,
+        'bike_id': fields.Integer
+    }
+
+    @marshal_with(resource_fields)
+    def get(self, bike_id):
+        """
+        Get all events for the given bike.
+        """
+        bike = Bike.query.get_or_404(bike_id)
+        events = bike.maintenance_events
+        return events
+
+    def post(self):
+        """
+        Create a new maintenance event.
+        """
+        data = request.get_json()
+
+        if data is None:
+            raise ClientDataError('Must include request datar')
+
+        event_date = data.get('date', None)
+        description = data.get('description', None)
+        note = data.get('note', None)
+        bike_id = data.get('bike_id', None)
+
+        if event_date is None:
+            raise ClientDataError('Must include date of event', 400)
+        try:
+            fmt_event_date = datetime.strptime(event_date, '%Y-%m-%d')
+        except ValueError:
+            raise ClientDataError('Date {} must be formatted YYYY-MM-DD'
+                    .format(event_date))
+        event_date = date(fmt_event_date.year, fmt_event_date.month,
+                fmt_event_date.day)
+
+        if description is None:
+            raise ClientDataError('Must include event description')
+
+        bike = Bike.query.get_or_404(bike_id)
+
+        event = MaintenanceEvent(event_date, description, note, bike)
+
+        try:
+            db.session.add(event)
+            db.session.commit()
+        except DataError:
+            return None, 400
+        except DataBaseError:
+            return None, 500
+
+        return {'id': event.id}
+
