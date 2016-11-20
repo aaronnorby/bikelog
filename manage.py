814753dc8650b0faca992c1b5cb6504247d5f898
Aaron Norby <aaron.norby@gmail.com>
2016-11-20 11:53:26 -0800

    Add shell script

--- manage.py
+++ manage.py
@@ -1,12 +1,17 @@
-from flask_script import Server, Manager
+from flask_script import Server, Shell, Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from bikelog import app
 from bikelog import db
+from bikelog import models
 
 migrate = Migrate(app, db)
 manager = Manager(app)
 
+def make_shell_context():
+    return dict(app=app, db=db, models=models)
+
+manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 manager.add_command('runserver', Server())
 
