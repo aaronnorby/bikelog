e73914d0a1260015cdfa46837e41adb7c340ed97
Aaron Norby <aaron.norby@gmail.com>
2016-11-19 16:40:19 -0800

    Some example curl commands

---
+++ curl_scripts.sh
@@ -0,0 +1,7 @@
+curl -i -X POST -H "Content-Type: application/json" -d '{"date": "2016-11-01-00-00", "description": "lube chain", "note":"triflow", "bike_id": 1}' "localhost:5000/api/maintenance_events"
+
+curl -i -X GET "localhost:5000/api/maintenance_events/1"
+
+curl -i -X POST -H "Content-Type: application/json" -d '{"name": "Trek", "purchased_at": "2015-03-28-00-00"}' "localhost:5000/api/bike"
+
+
